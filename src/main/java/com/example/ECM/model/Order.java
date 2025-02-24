91eb4b43f26d60c4f4e003f10b5b05af15f3ea76
bachbui003 <buivanbach003@gmail.com>
2025-02-24 11:08:04 +0700

    second_commit

--- src/main/java/com/example/ECM/model/Order.java
+++ src/main/java/com/example/ECM/model/Order.java
@@ -1,16 +1,19 @@
 package com.example.ECM.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Table(name = "orders")
 @Entity
+@Table(name = "orders")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "orderItems") // Tránh vòng lặp khi log
+@JsonIgnoreProperties({"orderItems"}) // Tránh vòng lặp khi serialize JSON
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +26,6 @@ public class Order {
     private BigDecimal totalPrice;
     private String status;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems;
-
 }
